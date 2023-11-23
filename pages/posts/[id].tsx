30849656aa6dc4d40bfac81b43b1e75ae02806d3
trukkerweeler <ken22008@byui.edu>
2023-11-22 18:03:28 -0700

    initial-typescript-push

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -3,23 +3,27 @@ import { getAllPostIds, getPostData } from '../../lib/posts';
 import Head from 'next/head';
 import Date from '../../components/date';
 import utilStyles from '../../styles/utils.module.css';
+import { GetStaticProps, GetStaticPaths, GetServerSideProps } from 'next';
 
-export async function getStaticProps({ params }) {
+
+export const getStaticProps = (async ({ params }) => {
   const postData = await getPostData(params.id);
   return {
     props: {
       postData,
     },
   };
-}
+}) as GetStaticProps;
 
-export async function getStaticPaths() {
+export const getStaticPaths = (async () => {
   const paths = getAllPostIds();
   return {
     paths,
     fallback: false,
   };
-}
+}) as GetStaticPaths;
+
+
 
 export default function Post({ postData }) {
     return (
